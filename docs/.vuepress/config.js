9ba20342a53da508120fe04eae482b464255e792
CnData07 <data07@126.com>
2022-04-16 10:53:55 +0800

    git

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,9 @@
 module.exports = {
     base:"/SvtccDocs/",
     title: 'Hello VuePress',
-    description: '关于四川交通职业技术学院的校园生活指南'
+    description: '关于四川交通职业技术学院的校园生活指南',
+    
+    themeConfig: {
+      lastUpdated: '更新时间', // string | boolean
+    }
   }
