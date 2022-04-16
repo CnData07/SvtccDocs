92b2ee130e241f33803bdf242c6e4760a025ba51
CnData07 <data07@126.com>
2022-04-17 01:49:37 +0800

    导航栏的logo还是不显示

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   base: "/SvtccDocs/",
   title: '校园生活指南',
+  logo: '/img/logo.png', // 导航栏logo
   description: '关于四川交通职业技术学院的校园生活指南',
 
   themeConfig: {
@@ -20,8 +21,8 @@ module.exports = {
   },
   // permalink: "/:year/:month/:day/:slug",  //永久链接
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-  logo: '/img/data.jpg', // 导航栏logo
-  repo: 'http://github.com/SvtccDocs/', // 导航栏右侧生成Github链接
+
+  // repo: 'http://github.com/SvtccDocs/', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
