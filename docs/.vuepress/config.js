72541569c790e7d77b3252310f8b2e747370b3d4
CnData07 <data07@126.com>
2022-06-15 11:27:13 +0800

    修改了一次bar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,9 +8,6 @@ module.exports = {
     logo: '/img/data.jpg', // 导航栏logo
     displayAllHeaders: true, // 默认值：false
 
-
-
-
     sidebar: {
       '/01.新生/': [
         '新生入校'
@@ -36,7 +33,6 @@ module.exports = {
     nav: [
       { text: '首页', link: '/' },
       { text: '新生', link: '/01.新生/新生入校' },
-      { text: '公共链接', link: '/02.公共链接/' },
       { text: '校园服务', link: '/03.校园服务/' },
       { text: '校园生活', link: '/04.校园生活/' },
       { text: '游戏列表', link: '/05.游戏列表/' , items: [
@@ -45,26 +41,20 @@ module.exports = {
           { text: 'Other', link: '/05.游戏列表/02/' }
         ]}
       ]},
-      { text: 'QQ频道', link: 'https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&appChannel=share&inviteCode=6jrMa&appChannel=share&businessType=9&from=246610&biz=ka' },
+      { text: 'QQ频道', link: 'https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&appChannel=share&inviteCode=6jrMa&appChannel=share&businessType=9&from=246610&biz=ka'},
     ],
   },
   // permalink: "/:year/:month/:day/:slug",  //永久链接
 
-  // repo: 'http://github.com/SvtccDocs/', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
-  // displayAllHeaders: true, // 默认值：false  设置为 true 来显示所有页面的标题链接：
+  // displayAllHeaders: true, 
+  // 默认值：false  设置为 true 来显示所有页面的标题链接：
 
-  //   // 监听文件变化并重新构建
+  // 监听文件变化并重新构建
   extraWatchFiles: [
     '.vuepress/config.js',
     '.vuepress/config/htmlModules.js',
   ],
-
-  // plugins: [
-  //   ["vuepress-plugin-auto-sidebar", {
-  //     // options
-  //   }]
-  // ]
 }
---
+++ docs/05.游戏列表/README.md
@@ -0,0 +1,25 @@
+---
+title: TopSky空岛服
+tags:
+  - TopSky
+  - topsky
+  - Top
+  - top
+  - Sky
+  - sky
+  - 空岛
+  - 服务器
+  - 空岛服
+  - 我的世界
+  - minecraft
+---
+
+## A
+
+## B
+
+## C
+
+## D
+
+### N
