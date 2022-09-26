3485bee70ecd8d5de22689a530f29eb4c1971882
CnData07 <data07@126.com>
2022-09-27 00:03:28 +0800

    yes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+# !/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run docs:build
+vuepress build docs
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 git push -f git@github.com:CnData07/SvtccDocs.git master:gh-pages
 
 cd -
